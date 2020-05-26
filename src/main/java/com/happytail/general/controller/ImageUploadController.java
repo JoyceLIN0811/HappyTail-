fe41e481cf8b68d28d398b9f521658beeaeb564f
JoyceLIN0811 <chihaya0811@gmail.com>
2020-05-26 09:22:47 +0800

    imgur_39

---
+++ src/main/java/com/happytail/general/controller/ImageUploadController.java
@@ -0,0 +1,46 @@
+package com.happytail.general.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.happytail.general.model.service.ImageUploadService;
+
+@RestController
+public class ImageUploadController {
+
+	@Autowired
+	private ImageUploadService imageUploadService;
+
+	@PostMapping("/uploadTopicImg")
+	Map<String, String> uploadTopicImg(@RequestParam("image") MultipartFile image, ModelMap model) throws IOException {
+
+		Map<String, String> map = new HashMap<>();
+		String imageUrl = imageUploadService.uploadSingleImage(image);
+
+		if (imageUrl != null) {
+			map.put("uploaded", "true");
+			map.put("url", "imageUrl");
+			System.out.println("imageUrl=" + imageUrl);
+		} else {
+			map.put("uploaded", "false");
+			map.put("url", "");
+			System.out.println("imageUrl: not exsit");
+		}
+
+		return map;
+	}
+
+}
